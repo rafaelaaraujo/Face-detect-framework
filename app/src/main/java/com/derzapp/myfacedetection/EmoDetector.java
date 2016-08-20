27f182e24e1dcf576e52e0087f4718f428d56139
Rafaela Araujo <Rafaela Araujo>
2016-08-20 14:58:06 -0300

    ajustes no android.mk

---
+++ app/src/main/java/com/derzapp/myfacedetection/EmoDetector.java
@@ -0,0 +1,11 @@
+package com.derzapp.myfacedetection;
+
+/**
+ * Created by rafaela on 20/08/2016.
+ */
+public class EmoDetector {
+
+    static {
+        System.loadLibrary("emoDetector");
+    }
+}
--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -37,7 +37,7 @@ include $(BUILD_SHARED_LIBRARY)
 #OPENCV_INSTALL_MODULES:=off
 #OPENCV_LIB_TYPE:=SHARED
 #include ../../sdk/native/jni/OpenCV.mk
-#LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp EmoDetector.cpp
+#LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp /EmoDetector.cpp
 #LOCAL_C_INCLUDES += $(LOCAL_PATH)
 #LOCAL_LDLIBS     += -llog -ldl
 #LOCAL_MODULE     := app
